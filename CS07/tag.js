ca7a3799700aa3d6cac6da2c87f6c48b609d82b5
snoopso <real@snoop.so>
2023-01-29 09:36:16 +0900

    Add CS07

---
+++ CS07/tag.js
@@ -0,0 +1,10 @@
+export class Tag {
+  constructor() {
+    this.position = null;
+    this.element = null;
+    this.attributes = null;
+    this.isOpened = false;
+    this.type = null;
+    this.children = null;
+  }
+}
