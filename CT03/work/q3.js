0640bcc2aab45a9e8223f1a7c8cdc948177b542f
snoopso <mingyeongso@mingyeongs-MacBook-Air.local>
2023-02-01 16:10:24 +0900

    Solve question 4

--- CT03/work/q3.js
+++ CT03/work/q3.js
@@ -3,6 +3,23 @@
 
 // using namespace std;
 
-export function solution(array, commands) {
+export function solution(s) {
+  let answer = '';
+  let arr = [];
+  s.split(' ').reduce((_, word) => {
+    arr.push(
+      word.split('').reduce((result, char, i) => {
+        if (i % 2 === 0) return result + char.toUpperCase();
+        return result + char.toLowerCase();
+      }, '')
+    );
+  }, '');
+  answer = arr.join(' ');
   return answer;
 }
+
+const print = (v) => console.log(solution(v));
+const check = (v, a) => console.log(solution(v) === a);
+
+print('try hello world');
+check('try hello world', 'TrY HeLlO WoRlD');
